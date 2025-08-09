9ef3720f07a75742dcef684265f7b006c01ed9e3
DoveDev <dovesbyte@gmail.com>
2025-08-09 21:46:44 +0100

    Create hello.py
    Readme's runnable code.

---
+++ hello.py
@@ -0,0 +1,11 @@
+# hello.py
+# Optional: Install pyfiglet: pip install pyfiglet
+
+import pyfiglet
+
+# ASCII
+banner = pyfiglet.figlet_format("Hello, World!")
+print(banner)
+
+# Note
+print("Welcome. Doesn't every journey start with a single print()? ⌨️")
